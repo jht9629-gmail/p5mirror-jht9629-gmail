379191ed54294b5d8e8e21ed4bac803c8d1541eb
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2023-12-29 01:57:05 -0500

    name+id

--- downloads/gen/unzip.sh
+++ downloads/gen/unzip.sh
@@ -1,193 +1,194 @@
 cd p5projects
 #
-# mo-storage-dashboard
-rm -r "mo-storage-dashboard"
-mkdir "mo-storage-dashboard"
-cd "mo-storage-dashboard"
-unzip "../../downloads/zips/mo-storage-dashboard"
+echo unzip 1 "mo-storage-dashboard-VWbtyZTnx"
+rm -rf "mo-storage-dashboard-VWbtyZTnx"
+mkdir "mo-storage-dashboard-VWbtyZTnx"
+cd "mo-storage-dashboard-VWbtyZTnx"
+unzip -q "../../downloads/zips/mo-storage-dashboard-VWbtyZTnx"
 cd ..
 #
-# pixel combo Layer
-rm -r "pixel combo Layer"
-mkdir "pixel combo Layer"
-cd "pixel combo Layer"
-unzip "../../downloads/zips/pixel combo Layer"
+echo unzip 2 "pixel combo Layer-dLk5kCeTO"
+rm -rf "pixel combo Layer-dLk5kCeTO"
+mkdir "pixel combo Layer-dLk5kCeTO"
+cd "pixel combo Layer-dLk5kCeTO"
+unzip -q "../../downloads/zips/pixel combo Layer-dLk5kCeTO"
 cd ..
 #
-# pixel combo
-rm -r "pixel combo"
-mkdir "pixel combo"
-cd "pixel combo"
-unzip "../../downloads/zips/pixel combo"
+echo unzip 3 "pixel combo-_BoYAq-PI"
+rm -rf "pixel combo-_BoYAq-PI"
+mkdir "pixel combo-_BoYAq-PI"
+cd "pixel combo-_BoYAq-PI"
+unzip -q "../../downloads/zips/pixel combo-_BoYAq-PI"
 cd ..
 #
-# pixel combo Next
-rm -r "pixel combo Next"
-mkdir "pixel combo Next"
-cd "pixel combo Next"
-unzip "../../downloads/zips/pixel combo Next"
+echo unzip 4 "pixel combo Next-b9tLAOHqq"
+rm -rf "pixel combo Next-b9tLAOHqq"
+mkdir "pixel combo Next-b9tLAOHqq"
+cd "pixel combo Next-b9tLAOHqq"
+unzip -q "../../downloads/zips/pixel combo Next-b9tLAOHqq"
 cd ..
 #
-# pixel combo bands
-rm -r "pixel combo bands"
-mkdir "pixel combo bands"
-cd "pixel combo bands"
-unzip "../../downloads/zips/pixel combo bands"
+echo unzip 5 "pixel combo bands-xvdjaQIhM"
+rm -rf "pixel combo bands-xvdjaQIhM"
+mkdir "pixel combo bands-xvdjaQIhM"
+cd "pixel combo bands-xvdjaQIhM"
+unzip -q "../../downloads/zips/pixel combo bands-xvdjaQIhM"
 cd ..
 #
-# pixel combo Next TIme millis aDelays
-rm -r "pixel combo Next TIme millis aDelays"
-mkdir "pixel combo Next TIme millis aDelays"
-cd "pixel combo Next TIme millis aDelays"
-unzip "../../downloads/zips/pixel combo Next TIme millis aDelays"
+echo unzip 6 "pixel combo Next TIme millis aDelays-tI8IX029X"
+rm -rf "pixel combo Next TIme millis aDelays-tI8IX029X"
+mkdir "pixel combo Next TIme millis aDelays-tI8IX029X"
+cd "pixel combo Next TIme millis aDelays-tI8IX029X"
+unzip -q "../../downloads/zips/pixel combo Next TIme millis aDelays-tI8IX029X"
 cd ..
 #
-# pixel combo Next TIme millis
-rm -r "pixel combo Next TIme millis"
-mkdir "pixel combo Next TIme millis"
-cd "pixel combo Next TIme millis"
-unzip "../../downloads/zips/pixel combo Next TIme millis"
+echo unzip 7 "pixel combo Next TIme millis-PPU_1IFRa"
+rm -rf "pixel combo Next TIme millis-PPU_1IFRa"
+mkdir "pixel combo Next TIme millis-PPU_1IFRa"
+cd "pixel combo Next TIme millis-PPU_1IFRa"
+unzip -q "../../downloads/zips/pixel combo Next TIme millis-PPU_1IFRa"
 cd ..
 #
-# pixel combo Next TIme frameCount
-rm -r "pixel combo Next TIme frameCount"
-mkdir "pixel combo Next TIme frameCount"
-cd "pixel combo Next TIme frameCount"
-unzip "../../downloads/zips/pixel combo Next TIme frameCount"
+echo unzip 8 "pixel combo Next TIme frameCount-4p4LPWvat"
+rm -rf "pixel combo Next TIme frameCount-4p4LPWvat"
+mkdir "pixel combo Next TIme frameCount-4p4LPWvat"
+cd "pixel combo Next TIme frameCount-4p4LPWvat"
+unzip -q "../../downloads/zips/pixel combo Next TIme frameCount-4p4LPWvat"
 cd ..
 #
-# soundFile
-rm -r "soundFile"
-mkdir "soundFile"
-cd "soundFile"
-unzip "../../downloads/zips/soundFile"
+echo unzip 9 "soundFile-8sM93vD0n"
+rm -rf "soundFile-8sM93vD0n"
+mkdir "soundFile-8sM93vD0n"
+cd "soundFile-8sM93vD0n"
+unzip -q "../../downloads/zips/soundFile-8sM93vD0n"
 cd ..
 #
-# 3.5 circleX a_len ui slider checkbox select copy
-rm -r "3.5 circleX a_len ui slider checkbox select copy"
-mkdir "3.5 circleX a_len ui slider checkbox select copy"
-cd "3.5 circleX a_len ui slider checkbox select copy"
-unzip "../../downloads/zips/3.5 circleX a_len ui slider checkbox select copy"
+echo unzip 10 "3.5 circleX a_len ui slider checkbox select copy-OCFOju6nO"
+rm -rf "3.5 circleX a_len ui slider checkbox select copy-OCFOju6nO"
+mkdir "3.5 circleX a_len ui slider checkbox select copy-OCFOju6nO"
+cd "3.5 circleX a_len ui slider checkbox select copy-OCFOju6nO"
+unzip -q "../../downloads/zips/3.5 circleX a_len ui slider checkbox select copy-OCFOju6nO"
 cd ..
 #
-# three columns hover v2
-rm -r "three columns hover v2"
-mkdir "three columns hover v2"
-cd "three columns hover v2"
-unzip "../../downloads/zips/three columns hover v2"
+echo unzip 11 "three columns hover v2-cX-O-25n"
+rm -rf "three columns hover v2-cX-O-25n"
+mkdir "three columns hover v2-cX-O-25n"
+cd "three columns hover v2-cX-O-25n"
+unzip -q "../../downloads/zips/three columns hover v2-cX-O-25n"
 cd ..
 #
-# n columns hover v3
-rm -r "n columns hover v3"
-mkdir "n columns hover v3"
-cd "n columns hover v3"
-unzip "../../downloads/zips/n columns hover v3"
+echo unzip 12 "n columns hover v3-Tao-6oHD0"
+rm -rf "n columns hover v3-Tao-6oHD0"
+mkdir "n columns hover v3-Tao-6oHD0"
+cd "n columns hover v3-Tao-6oHD0"
+unzip -q "../../downloads/zips/n columns hover v3-Tao-6oHD0"
 cd ..
 #
-# three columns hover v1
-rm -r "three columns hover v1"
-mkdir "three columns hover v1"
-cd "three columns hover v1"
-unzip "../../downloads/zips/three columns hover v1"
+echo unzip 13 "three columns hover v1-hLi5Fdtxo"
+rm -rf "three columns hover v1-hLi5Fdtxo"
+mkdir "three columns hover v1-hLi5Fdtxo"
+cd "three columns hover v1-hLi5Fdtxo"
+unzip -q "../../downloads/zips/three columns hover v1-hLi5Fdtxo"
 cd ..
 #
-# Patch neem by bigmouthflower -- jht9629-gmail remix
-rm -r "Patch neem by bigmouthflower -- jht9629-gmail remix"
-mkdir "Patch neem by bigmouthflower -- jht9629-gmail remix"
-cd "Patch neem by bigmouthflower -- jht9629-gmail remix"
-unzip "../../downloads/zips/Patch neem by bigmouthflower -- jht9629-gmail remix"
+echo unzip 14 "Patch neem by bigmouthflower - jht9629-gmail remix-CibQhizdZ"
+rm -rf "Patch neem by bigmouthflower - jht9629-gmail remix-CibQhizdZ"
+mkdir "Patch neem by bigmouthflower - jht9629-gmail remix-CibQhizdZ"
+cd "Patch neem by bigmouthflower - jht9629-gmail remix-CibQhizdZ"
+unzip -q "../../downloads/zips/Patch neem by bigmouthflower - jht9629-gmail remix-CibQhizdZ"
 cd ..
 #
-# Patch neem by bigmouthflower
-rm -r "Patch neem by bigmouthflower"
-mkdir "Patch neem by bigmouthflower"
-cd "Patch neem by bigmouthflower"
-unzip "../../downloads/zips/Patch neem by bigmouthflower"
+echo unzip 15 "Patch neem by bigmouthflower-zpk0FK0KZ"
+rm -rf "Patch neem by bigmouthflower-zpk0FK0KZ"
+mkdir "Patch neem by bigmouthflower-zpk0FK0KZ"
+cd "Patch neem by bigmouthflower-zpk0FK0KZ"
+unzip -q "../../downloads/zips/Patch neem by bigmouthflower-zpk0FK0KZ"
 cd ..
 #
-# MazeSpin
-rm -r "MazeSpin"
-mkdir "MazeSpin"
-cd "MazeSpin"
-unzip "../../downloads/zips/MazeSpin"
+echo unzip 16 "MazeSpin-FuOH_EE4"
+rm -rf "MazeSpin-FuOH_EE4"
+mkdir "MazeSpin-FuOH_EE4"
+cd "MazeSpin-FuOH_EE4"
+unzip -q "../../downloads/zips/MazeSpin-FuOH_EE4"
 cd ..
 #
-# mouse ellipse
-rm -r "mouse ellipse"
-mkdir "mouse ellipse"
-cd "mouse ellipse"
-unzip "../../downloads/zips/mouse ellipse"
+echo unzip 17 "mouse ellipse-fiaNTD9Nj"
+rm -rf "mouse ellipse-fiaNTD9Nj"
+mkdir "mouse ellipse-fiaNTD9Nj"
+cd "mouse ellipse-fiaNTD9Nj"
+unzip -q "../../downloads/zips/mouse ellipse-fiaNTD9Nj"
 cd ..
 #
-# mouse rect
-rm -r "mouse rect"
-mkdir "mouse rect"
-cd "mouse rect"
-unzip "../../downloads/zips/mouse rect"
+echo unzip 18 "mouse rect-3IubAIXOX"
+rm -rf "mouse rect-3IubAIXOX"
+mkdir "mouse rect-3IubAIXOX"
+cd "mouse rect-3IubAIXOX"
+unzip -q "../../downloads/zips/mouse rect-3IubAIXOX"
 cd ..
 #
-# Youtube Player No API copy
-rm -r "Youtube Player No API copy"
-mkdir "Youtube Player No API copy"
-cd "Youtube Player No API copy"
-unzip "../../downloads/zips/Youtube Player No API copy"
+echo unzip 19 "Youtube Player No API copy-eYD39ApZp"
+rm -rf "Youtube Player No API copy-eYD39ApZp"
+mkdir "Youtube Player No API copy-eYD39ApZp"
+cd "Youtube Player No API copy-eYD39ApZp"
+unzip -q "../../downloads/zips/Youtube Player No API copy-eYD39ApZp"
 cd ..
 #
-# spiral walk
-rm -r "spiral walk"
-mkdir "spiral walk"
-cd "spiral walk"
-unzip "../../downloads/zips/spiral walk"
+echo unzip 20 "spiral walk-zaAsh0DZt"
+rm -rf "spiral walk-zaAsh0DZt"
+mkdir "spiral walk-zaAsh0DZt"
+cd "spiral walk-zaAsh0DZt"
+unzip -q "../../downloads/zips/spiral walk-zaAsh0DZt"
 cd ..
 #
-# maze tiles bits
-rm -r "maze tiles bits"
-mkdir "maze tiles bits"
-cd "maze tiles bits"
-unzip "../../downloads/zips/maze tiles bits"
+echo unzip 21 "maze tiles bits-IagYeywkY"
+rm -rf "maze tiles bits-IagYeywkY"
+mkdir "maze tiles bits-IagYeywkY"
+cd "maze tiles bits-IagYeywkY"
+unzip -q "../../downloads/zips/maze tiles bits-IagYeywkY"
 cd ..
 #
-# maze tiles count
-rm -r "maze tiles count"
-mkdir "maze tiles count"
-cd "maze tiles count"
-unzip "../../downloads/zips/maze tiles count"
+echo unzip 22 "maze tiles count-abgeEnTyf"
+rm -rf "maze tiles count-abgeEnTyf"
+mkdir "maze tiles count-abgeEnTyf"
+cd "maze tiles count-abgeEnTyf"
+unzip -q "../../downloads/zips/maze tiles count-abgeEnTyf"
 cd ..
 #
-# maze tiles pause
-rm -r "maze tiles pause"
-mkdir "maze tiles pause"
-cd "maze tiles pause"
-unzip "../../downloads/zips/maze tiles pause"
+echo unzip 23 "maze tiles pause-i2hCaC36l"
+rm -rf "maze tiles pause-i2hCaC36l"
+mkdir "maze tiles pause-i2hCaC36l"
+cd "maze tiles pause-i2hCaC36l"
+unzip -q "../../downloads/zips/maze tiles pause-i2hCaC36l"
 cd ..
 #
-# IMS - Truchet Vamoss copy
-rm -r "IMS - Truchet Vamoss copy"
-mkdir "IMS - Truchet Vamoss copy"
-cd "IMS - Truchet Vamoss copy"
-unzip "../../downloads/zips/IMS - Truchet Vamoss copy"
+echo unzip 24 "IMS - Truchet Vamoss copy-wDfyiYikE"
+rm -rf "IMS - Truchet Vamoss copy-wDfyiYikE"
+mkdir "IMS - Truchet Vamoss copy-wDfyiYikE"
+cd "IMS - Truchet Vamoss copy-wDfyiYikE"
+unzip -q "../../downloads/zips/IMS - Truchet Vamoss copy-wDfyiYikE"
 cd ..
 #
-# truchet tiles array
-rm -r "truchet tiles array"
-mkdir "truchet tiles array"
-cd "truchet tiles array"
-unzip "../../downloads/zips/truchet tiles array"
+echo unzip 25 "truchet tiles array-EfQDCJ5aR"
+rm -rf "truchet tiles array-EfQDCJ5aR"
+mkdir "truchet tiles array-EfQDCJ5aR"
+cd "truchet tiles array-EfQDCJ5aR"
+unzip -q "../../downloads/zips/truchet tiles array-EfQDCJ5aR"
 cd ..
 #
-# truchet tiles rotate
-rm -r "truchet tiles rotate"
-mkdir "truchet tiles rotate"
-cd "truchet tiles rotate"
-unzip "../../downloads/zips/truchet tiles rotate"
+echo unzip 26 "truchet tiles rotate-mxUEvOhbk"
+rm -rf "truchet tiles rotate-mxUEvOhbk"
+mkdir "truchet tiles rotate-mxUEvOhbk"
+cd "truchet tiles rotate-mxUEvOhbk"
+unzip -q "../../downloads/zips/truchet tiles rotate-mxUEvOhbk"
 cd ..
 #
-# truchet tiles pause
-rm -r "truchet tiles pause"
-mkdir "truchet tiles pause"
-cd "truchet tiles pause"
-unzip "../../downloads/zips/truchet tiles pause"
+echo unzip 27 "truchet tiles pause-LnPplI2CR"
+rm -rf "truchet tiles pause-LnPplI2CR"
+mkdir "truchet tiles pause-LnPplI2CR"
+cd "truchet tiles pause-LnPplI2CR"
+unzip -q "../../downloads/zips/truchet tiles pause-LnPplI2CR"
 cd ..
 cd ..
 # remove redundant p5.js p5.sound.min.js
-rm p5projects/*/p5.*
+rm -f p5projects/*/p5.*
+echo
