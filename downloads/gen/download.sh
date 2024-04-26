80f86f76800c5fa1167ca1809edc4351d43fac3e
John Henry Thompson <jht9629@nyu.edu>
2024-04-26 05:07:31 -0400

    29 sketches 2024-04-19

--- downloads/gen/download.sh
+++
@@ -1,2 +0,0 @@
-echo download 1 "video radial perlin pix-Aap84EcU_"
-curl -s https://editor.p5js.org/editor/projects/Aap84EcU_/zip -o "/Users/jht2/Documents/projects/2024/p5mo/p5mirror-jht9629-gmail/downloads/zips/video radial perlin pix-Aap84EcU_.zip"
--- downloads/gen/unzip.sh
+++ downloads/gen/unzip.sh
@@ -1,11 +1,4 @@
 cd "/Users/jht2/Documents/projects/2024/p5mo/p5mirror-jht9629-gmail/downloads/../p5projects"
-#
-echo unzip 1 "video radial perlin pix-Aap84EcU_"
-rm -rf "./video radial perlin pix-Aap84EcU_"
-mkdir "./video radial perlin pix-Aap84EcU_"
-pushd "./video radial perlin pix-Aap84EcU_" > /dev/null
-unzip -q "../../downloads/zips/video radial perlin pix-Aap84EcU_"
-popd > /dev/null
 
 cd ..
 # remove redundant p5.js p5.sound.min.js
